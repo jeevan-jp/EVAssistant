586e1b8facfee8a428d46a984d18cc61c0b269fc
mohit <mjain8156@gmail.com>
2018-08-27 00:01:04 +0530

    Still working

--- backend-node/index.js
+++ backend-node/index.js
@@ -4,7 +4,16 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 var logger = require('morgan');
+
+var mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://localhost/dbdata', { useMongoClient: true });
+
+require("./models/data");
 
 var dataRouter = require('./routes/data');
 
@@ -13,29 +22,10 @@ var session = require('express-session');
 
 var flash = require('connect-flash');
 
-var mongoose = require('mongoose');
-
-mongoose.Promise = global.Promise;
-
-mongoose.connect('mongodb://localhost/dbdata', { useMongoClient: true });
-
-require("./models/Data");
 
 
 
 var app = express();
-
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/data', dataRouter);
-
 
 app.use(session({ cookie: { maxAge: 60000 }, 
 
@@ -44,12 +34,25 @@ app.use(session({ cookie: { maxAge: 60000 },
     resave: false, 
 
     saveUninitialized: false}));
-    app.use(function(req, res, next) {
-        next(createError(404));
-      });
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(flash());
+app.use('/data', dataRouter);
+
+
+
+    
       
       // error handler
-      app.use(function(err, req, res, next) {
+app.use(function(err, req, res, next) {
         // set locals, only providing error in development
         res.locals.message = err.message;
         res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -59,5 +62,5 @@ app.use(session({ cookie: { maxAge: 60000 },
         res.render('error');
       });
       
-      module.exports = app;
+ module.exports = app;
         
--- backend-node/routes/data.js
+++ backend-node/routes/data.js
@@ -17,4 +17,46 @@ router.get('/', function(req, res, next) {
 
     res.render('index', { title: 'Import CSV file using NodeJS' });
 
-})
+}).get('/import', function(req, res, next) {
+
+    var  products  = []
+    var csvStream = csv()
+        .on("data", function(data){
+         
+         var item = new Data({
+              SNo: data[0] ,
+              Concession: data[1]   ,
+              Area: data[2],
+              sitecode: data[3],
+              stopName:data[4],
+              RoadName:data[5],
+              Towards:data[6] ,
+              coordinates:data[7]
+         });
+         
+          item.save(function(error){
+            console.log(item);
+              if(error){
+                   throw error;
+              }
+          }); 
+
+    }).on("end", function(){
+
+    });
+  
+    stream.pipe(csvStream);
+    res.json({success : "Data imported successfully.", status : 200});
+     
+  }).get('/fetchdata', function(req, res, next) {
+    
+    Product.find({}, function(err, docs) {
+        if (!err){ 
+            res.json({success : "Updated Successfully", status : 200, data: docs});
+        } else { 
+            throw err;
+        }
+    });
+  
+});
+module.exports = router;
